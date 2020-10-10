0429cdb9a2682e5f6e0c3d5101c8ee9d4ac9948b
Dhruv <dhruvbandaria@hotmail.com>
2020-10-10 10:50:17 -0400

    Initial commit

---
+++ app/src/main/java/dhruv/bandaria/s301102928/BandariaHomeTypesActivity.java
@@ -0,0 +1,14 @@
+package dhruv.bandaria.s301102928;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BandariaHomeTypesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bandaria_home_types);
+    }
+}
