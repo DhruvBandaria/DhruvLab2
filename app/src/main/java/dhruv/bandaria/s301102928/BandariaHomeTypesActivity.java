08e2984bb71ceed49f1448cbd827582dfd708f09
Dhruv <dhruvbandaria@hotmail.com>
2020-10-12 11:01:02 -0400

    Added Hometypes fragments

--- app/src/main/java/dhruv/bandaria/s301102928/BandariaHomeTypesActivity.java
+++ app/src/main/java/dhruv/bandaria/s301102928/BandariaHomeTypesActivity.java
@@ -2,6 +2,9 @@ package dhruv.bandaria.s301102928;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +13,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class BandariaHomeTypesActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +30,38 @@ public class BandariaHomeTypesActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        BandariaAppartmentFragment frag1 = new BandariaAppartmentFragment();
+        BandariaDetachedHome frag2=new BandariaDetachedHome();
+        BandariaSemiDetachedFragment frag3=new BandariaSemiDetachedFragment();
+        BandariaCondominiumFragment frag4 = new BandariaCondominiumFragment();
+        BandariaTownHouse frag5 = new BandariaTownHouse();
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction;
         switch (item.getItemId()){
             case R.id.item1:
-                Toast.makeText(getApplicationContext(),"item1",Toast.LENGTH_SHORT).show();
+                fragmentTransaction=fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer,frag1,"Demo Fragment");
+                fragmentTransaction.commit();
                 break;
             case R.id.item2:
-                Toast.makeText(getApplicationContext(),"item2",Toast.LENGTH_SHORT).show();
+                fragmentTransaction=fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer,frag2,"Demo Fragment");
+                fragmentTransaction.commit();
                 break;
             case R.id.item3:
-                Toast.makeText(getApplicationContext(),"item3",Toast.LENGTH_SHORT).show();
+                fragmentTransaction=fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer,frag3,"Demo Fragment");
+                fragmentTransaction.commit();
                 break;
             case R.id.item4:
-                Toast.makeText(getApplicationContext(),"item4",Toast.LENGTH_SHORT).show();
+                fragmentTransaction=fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer,frag4,"Demo Fragment");
+                fragmentTransaction.commit();
                 break;
             case R.id.item5:
-                Toast.makeText(getApplicationContext(),"item5",Toast.LENGTH_SHORT).show();
+                fragmentTransaction=fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer,frag5,"Demo Fragment");
+                fragmentTransaction.commit();
                 break;
         }
         return super.onOptionsItemSelected(item);
